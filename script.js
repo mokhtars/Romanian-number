2179d3b857c8eac516b63cdc7363ae3f90f35064
mokhtars <33429910+mokhtars@users.noreply.github.com>
2019-04-04 00:18:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
-$(function() {
-    // GET/READ
-    $('#get-button').on('click', function() {
-        $.ajax({
-            url: '/romain',
-            contentType: 'application/json',
-            success: function(response) {
-                var tbodyEl = $('tbody');
-                tbodyEl.html('');
-                    tbodyEl.append('\
-                        <tr>\
-                            <th class="id" >' + 'Votre numéro est' + '</th>\
-                            <td><input type="text" class="name" value="' + response.nbRomain + '"></td>\
-                            <tr>\
-                    ');
-                
-            }
-        });
-    });
-
-    // CREATE/POST
-    $('#create-form').on('submit', function(event) {
-        event.preventDefault();
-
-        var createInput = $('#create-input');
-        $.ajax({
-            url: '/romain',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ name: createInput.val() }),
-            success: response => {
-                createInput.val('');
-                $('#get-button').click();
-            }
-        });
-    });
-});
+var es = new EventSource("http://localhost:3000/")
+es.onmessage = event =>{
+    document.getElementById('rom').innerHTML = evemt.data;
+}
